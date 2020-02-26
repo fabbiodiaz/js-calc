5d6a0b0e0594df3ac057ac4d0a0d6d210f6d35c6
Fábio Dias <fabbiodiaz@gmail.com>
2020-02-26 11:12:52 -0300

    Ajuste na interface // lógica de cálculo funcionando

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
-const inputScreen = document.getElementById("input-screen")
-const outputScreen = document.getElementById("output-screen")
+//default display
+const display = document.getElementById("display")
 
+//default arithimetic operations
 operations = {
-    plus:(firstNum,secondNum) => firstNum + secondNum,
-    minimus: (firstNum,secondNum) => firstNum - secondNum,
-    mult: (firstNum,secondNum) => firstNum * secondNum,
-    divide: (firstNum,secondNum) => firstNum / secondNum,
-    equals:(flag,firstNum,secondNum) => operations[flag](firstNum,secondNum)
-    }
+    plus:(firstNum=0,secondNum=0) => firstNum + secondNum,
+    minimus: (firstNum=0,secondNum=0) => firstNum - secondNum,
+    mult: (firstNum=1,secondNum=1) => firstNum * secondNum,
+    divide: (firstNum=1,secondNum=1) => firstNum / secondNum,
+    equals:(flag,firstNum,secondNum) => operations[flag](firstNum,secondNum).toString().length > 8 ? null : operations[flag](firstNum,secondNum)
+}
 
+// Random acess memory and its methods
 const memory = { flag:"", xRegister:"", yRegister:"",
 
-    inputNumber:data => {
-        
-        if (memory.flag) {
-            memory.yRegister=memory.xRegister
-            memory.xRegister=""
-        }
+inputNumber:data => {
+    
+    if (memory.flag && !memory.yRegister) {
+        memory.yRegister=memory.xRegister
+        memory.xRegister=""
+    }
+
+    if (memory.xRegister.toString().length < 8) {
         data = memory.xRegister.toString().concat(data)
-        memory.xRegister = parseInt(data)
-        //screenInput(memory.xRegister,inputScreen)
+            memory.xRegister = parseInt(data)
+        }
     },
 
-    inputFlag:data => {            
-        if (memory.flag && memory.flag!="equals") {
+    inputFlag:data => {
+        if (memory.flag) {
             memory.xRegister = operations.equals(memory.flag,memory.yRegister,memory.xRegister)
             memory.yRegister = ""
+            memory.flag=""
         }
-        memory.flag=data
+        if(data!="equals") {
+            memory.flag=data
+        }
         
     },
 
@@ -42,13 +49,13 @@ const memory = { flag:"", xRegister:"", yRegister:"",
             memory.command (data)
         }
 
-        //screenInput(memory.xRegister,outputScreen)
+        displayInput(memory.xRegister,display)
     }
 }
 
 
-const screenInput = (input, screen) => {
-    screen.innerHTML = input
+const displayInput = (input, display) => {
+    display.innerHTML = input
 }
 
 keys = document.querySelectorAll(".number, .command, .operation")
